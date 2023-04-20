52323ade8d708edfda737004829f2f839aab645a
yoponchik <57009810+yoponchik@users.noreply.github.com>
2023-04-20 19:14:41 +0900

    Working Progress: Get Position, Velocity, Orientation, and Angular Velocity
    -Export FString to CSV File

---
+++ Source/GestureRecognition/Public/GestureRecognitionPlayer.h
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "InputMappingContext.h"
+#include "GestureRecognitionPlayer.generated.h"
+
+const float RECORD_FREQUENCY_SECONDS = 0.1f;
+const FString FILENAME = TEXT("MotionControllerData.csv");
+
+UCLASS()
+class GESTURERECOGNITION_API AGestureRecognitionPlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AGestureRecognitionPlayer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+#pragma region Input
+	UPROPERTY(EditAnywhere, Category = "Player Settings | Inputs | Movement")
+	class UInputMappingContext* IMC_VRInput;
+
+	UPROPERTY(EditAnywhere, Category = "Player Settings | Inputs | Movement")
+	class UInputAction* IA_RecordMovement;
+
+#pragma endregion Input
+
+	UFUNCTION()
+	void OnActionRecordMovement();
+
+	bool bIsRecord = false;
+
+	void Record();
+
+#pragma region Camera, Controllers & Hands
+	UPROPERTY(EditAnywhere, Category = "Player Settings | VR Settings")
+	class UCameraComponent* VRCamera;
+
+	UPROPERTY(EditAnywhere, Category = "Player Settings | VR Settings")
+	class UMotionControllerComponent* LeftHand;
+	UPROPERTY(EditAnywhere, Category = "Player Settings | VR Settings")
+	class UMotionControllerComponent* RightHand;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Settings | VR Settings")
+	class USkeletalMeshComponent* LeftHandMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Settings | VR Settings")
+	class USkeletalMeshComponent* RightHandMesh;
+#pragma endregion Controllers & Hands
+
+	UPROPERTY(EditAnywhere)
+	class USphereComponent* SphereDetectorRight;
+	UPROPERTY(EditAnywhere)
+	class USphereComponent* SphereDetectorLeft;
+	
+	UPROPERTY()
+	class APlayerController* PlayerController;
+
+	FVector GetRelativeLocation(UMotionControllerComponent* InMotionController);
+	FQuat GetRelativeRotation(UMotionControllerComponent* InMotionController);
+	FVector GetControllerAngularVelocity(USphereComponent* InSphereDetector);
+	FVector GetControllerVelocity(USphereComponent* InSphereDetector);
+
+	FVector PreviousVelocity;
+	
+};
