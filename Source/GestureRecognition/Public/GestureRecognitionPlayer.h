d941e4a69c4d0b0d0bed696f0a02e6e18c9d7f6e
yoponchik <57009810+yoponchik@users.noreply.github.com>
2023-04-27 15:01:17 +0900

    Organize and Comment Code

--- Source/GestureRecognition/Public/GestureRecognitionPlayer.h
+++ Source/GestureRecognition/Public/GestureRecognitionPlayer.h
@@ -31,6 +31,9 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY()
+	class APlayerController* PlayerController;
+	
 #pragma region Input
 	UPROPERTY(EditAnywhere, Category = "Player Settings | Inputs | Movement")
 	class UInputMappingContext* IMC_VRInput;
@@ -39,19 +42,6 @@ public:
 	class UInputAction* IA_RecordMovement;
 
 #pragma endregion Input
-
-	UFUNCTION()
-	void OnActionRecordMovement();
-
-	bool bIsRecord = false;
-
-	void Record();
-
-	int32 CurrentFileIndex = 0;
-	FString CSVHeaders = "Time,Right Hand Position X,Right Hand Position Y,Right Hand Position Z,Right Hand Velocity X,Right Hand Velocity Y,Right Hand Velocity Z,Right Hand Orientation X,Right Hand Orientation Y,Right Hand Orientation Z,Right Hand Angular Velocity X,Right Hand Angular Velocity Y,Right Hand Angular Velocity Z,Right Hand Angular Velocity W, \n";
-
-	float RecordingStartTime = 0.0f;
-	
 
 #pragma region Camera, Controllers & Hands
 	UPROPERTY(EditAnywhere, Category = "Player Settings | VR Settings")
@@ -66,21 +56,36 @@ public:
 	class USkeletalMeshComponent* LeftHandMesh;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Settings | VR Settings")
 	class USkeletalMeshComponent* RightHandMesh;
-#pragma endregion Controllers & Hands
 
+	//Components used to extract controller data
 	UPROPERTY(EditAnywhere)
 	class USphereComponent* SphereDetectorRight;
 	UPROPERTY(EditAnywhere)
 	class USphereComponent* SphereDetectorLeft;
-	
-	UPROPERTY()
-	class APlayerController* PlayerController;
+#pragma endregion Controllers & Hands
 
+
+	UFUNCTION()
+	void OnActionRecordMovement();
+
+	void Record();
+
+
+	int32 CurrentFileIndex = 0;
+	FString CSVHeaders = "Time,Right Hand Position X,Right Hand Position Y,Right Hand Position Z,Right Hand Velocity X,Right Hand Velocity Y,Right Hand Velocity Z,Right Hand Orientation X,Right Hand Orientation Y,Right Hand Orientation Z,Right Hand Angular Velocity X,Right Hand Angular Velocity Y,Right Hand Angular Velocity Z,Right Hand Angular Velocity W, \n";
+
+	
+	float RecordingStartTime = 0.0f;
+	bool bIsRecord = false;
+
+
+	//For getting controller data 
 	FVector GetRelativeLocation(UMotionControllerComponent* InMotionController);
 	FQuat GetRelativeRotation(UMotionControllerComponent* InMotionController);
 	FVector GetControllerAngularVelocity(USphereComponent* InSphereDetector);
 	FVector GetControllerVelocity(USphereComponent* InSphereDetector);
 
 	FVector PreviousVelocity;
-	
+
+
 };
