fa6e1b8b82203522acaa0265827f6c1ecf6f9f68
yoponchik <57009810+yoponchik@users.noreply.github.com>
2023-04-30 16:30:49 +0900

    Update GestureRecognitionPlayer.h
    Exposed SampleRateSeconds to Editor

--- Source/GestureRecognition/Public/GestureRecognitionPlayer.h
+++ Source/GestureRecognition/Public/GestureRecognitionPlayer.h
@@ -8,7 +8,7 @@
 #include "InputMappingContext.h"
 #include "GestureRecognitionPlayer.generated.h"
 
-const float RECORD_FREQUENCY_SECONDS = 0.1f;
+
 const FString FILENAME = TEXT("MotionControllerData.csv");
 
 UCLASS()
@@ -84,6 +84,8 @@ public:
 	int32 CurrentFileIndex = 0;
 	FString CSVHeaders = "Time,Relevant Segment,Right Hand Position X,Right Hand Position Y,Right Hand Position Z,Right Hand Velocity X,Right Hand Velocity Y,Right Hand Velocity Z,Right Hand Orientation X,Right Hand Orientation Y,Right Hand Orientation Z,Right Hand Angular Velocity X,Right Hand Angular Velocity Y,Right Hand Angular Velocity Z,Right Hand Angular Velocity W\n";
 
+ UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Record Settings")
+ float RecordSampleRateSeconds = 0.1f;
 	float RecordingStartTime = 0.0f;
 	bool bIsRecord = false;			
 
